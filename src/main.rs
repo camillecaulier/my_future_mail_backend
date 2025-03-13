94d3a2bbd01b92a36096af55e21e8ddd0a0ce4e7
camillecaulier <camillecaulier.cc@gmail.com>
2025-03-13 23:55:58 +0100

    hello world working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use axum::serve;
 use my_future_mail_backend::create_app;
 use std::net::SocketAddr;
 use tokio::net::TcpListener;
@@ -5,11 +6,10 @@ use tokio::net::TcpListener;
 #[tokio::main]
 async fn main() {
     let app = create_app();
+
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     println!("Server running at http://{}", addr);
 
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+    let listener = TcpListener::bind(addr).await.unwrap();
+    serve(listener, app.into_make_service()).await.unwrap();
 }
