d6fbdae986d0c5acfa0d960dcc955c880ec295c7
camillecaulier <camillecaulier.cc@gmail.com>
2025-03-16 16:27:56 +0100

    creating basic api test

--- tests/hello_world.rs
+++ tests/hello_world.rs
@@ -1,12 +1,12 @@
+use axum::body::Body;
+use axum::body::to_bytes;
 use axum::http::{Request, StatusCode};
-use hyper::Body;
 use my_future_mail_backend::create_app;
 use tower::ServiceExt; // Required for testing // Import app from `lib.rs`
 
 #[tokio::test]
 async fn test_hello_world() {
     let app = create_app();
-
     let response = app
         .oneshot(Request::builder().uri("/").body(Body::empty()).unwrap())
         .await
@@ -14,6 +14,6 @@ async fn test_hello_world() {
 
     assert_eq!(response.status(), StatusCode::OK);
 
-    let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
+    let body = to_bytes(response.into_body(), 1000).await.unwrap();
     assert_eq!(body, "Hello, World!");
 }
