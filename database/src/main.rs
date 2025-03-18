aae361d566da673077d2306f1c268d2217eb33ed
camillecaulier <camillecaulier.cc@gmail.com>
2025-03-18 23:27:59 +0100

    working dummy connection to database

--- database/src/main.rs
+++ database/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use dotenv::dotenv;
+use sqlx::{Pool, Postgres};
+use std::env;
+
+#[tokio::main]
+async fn main() -> Result<(), sqlx::Error> {
+    dotenv().ok();
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    // Connect to your local database
+    let pool: Pool<Postgres> = sqlx::PgPool::connect(&database_url).await?;
+
+    // Your code to interact with the database here.
+    println!("Connected to the local database successfully!");
+
+    Ok(())
 }
